3ed9eb0ed4a3610ff4b02ad323e4e93da1a39e57
Anthony Romano <heyitsanthony@users.noreply.github.com>
2017-11-28 15:25:43 -0800

    grammars: add ICFP02 paper grammars
    Some baseline examples.

---
+++ grammars/icfp02/doc.go
@@ -0,0 +1,4 @@
+// Package icfp02 implements PEG grammars adapted from the functional pearl packrat parser paper [icfp02].
+//
+// [icfp02]: Bryan Ford. "Packrat Parsing: Simple, Powerful, Lazy, Linear Time." ICFP'02. October, 2002.
+package icfp02
